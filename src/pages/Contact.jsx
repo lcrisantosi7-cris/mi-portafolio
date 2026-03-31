1ae9fa0e8e1db9c24a0923af294c4793482025a9
lcrisantosi7-cris <lcrisantosi7@gmail.com>
2026-03-31 17:30:12 -0500

    Cambio de URL

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,7 @@ import {
   CheckCircle2, Loader2, ArrowRight, Copy, Terminal, Wifi 
 } from 'lucide-react'
 
-const BACKEND_URL = 'https://portfolio-backend-htox.onrender.com'
+const BACKEND_URL = 'https://mi-portafolio-yx3r.onrender.com'
 
 const Contact = () => {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' })
